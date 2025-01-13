00cce327c670385dca1ff0addaeb5e291a178a5c
Georgiy Zakirov <granmawinter@icloud.com>
2025-01-14 01:04:54 +0400

    Fix changes

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import requests
 import json
 from dadata import Dadata
 from telebot import types
+from keys import TOKEN, SECRET_KEY, DADATA_TOKEN
 
-bot = telebot.TeleBot('7731839807:AAG3gKELPKJdDu0WgHPpg-RApo04GxnwIBU')
-dadata_token = '877d125aeebbc6f111a42cb4bf7f21fc9bb27a3b'
-secret = "2aea8167ef719c5f8a4a0302dd82ca85da2f1126"
+# configuring the bot and connecting to the api
+
+
+bot = telebot.TeleBot(TOKEN)
+dadata_token = DADATA_TOKEN
+secret = SECRET_KEY
 
 my_dadata = Dadata(dadata_token, secret)
 
@@ -29,17 +33,23 @@ def return_org(message):
     link = f"https://www.rusprofile.ru/search?query={inn}"
     bot.reply_to(message, f"Название организации: {org_name}\nЮридический адрес: {value}\nИНН: {inn}\n{link}")
 
-def return_bik(message):
-    inn = message.text
-    headers = {'Content-Type': 'application/json', 'Authorization': 'Token {}'.format(dadata_token)}
-    response = requests.post('http://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/bank', headers= headers, json={inn})
-    data = response.json()
-
-    if not data['suggestions']:
-        bot.send_message(message.chat.id, 'ИНН не найден')
-        return
-
-    bik = data['suggestions'][0]
-    bot.reply_to(message, f'это БИК : {bik}')
-
 bot.polling(none_stop=True)
+
+
+# bik module in progress
+
+# @bot.message_handler(func=lambda message: True)
+# def return_bik(message):
+#     inn = message.text
+#     headers = {'Content-Type': 'application/json', 'Authorization': 'Token {}'.format(dadata_token)}
+#     response = requests.post('http://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/bank', headers= headers, json={"query": inn})
+#     print(f"response: {response}")
+#     data = response.json()
+#
+#
+#     if not data['suggestions']:
+#         bot.send_message(message.chat.id, 'ИНН не найден')
+#         return
+#
+#     bik = data['suggestions'][0]['data']['bic']
+#     bot.reply_to(message, f'БИК : {bik}')
