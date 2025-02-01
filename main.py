d8fd146b912792f2204d434e01e8aab073700d4e
Georgiy Zakirov <granmawinter@icloud.com>
2025-02-01 21:41:59 +0400

    fix keys

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import requests
 import json
 from dadata import Dadata
 from telebot import types
-from keys import TOKEN, SECRET_KEY, DADATA_TOKEN
+# from keys import TOKEN, SECRET_KEY, DADATA_TOKEN
+import os
+
+
+# getting environment variables
+TOKEN = os.getenv('BOT_TOKEN_GET_INN')
+SECRET_KEY = os.getenv('SECRET_KEY_GET_INN')
+DADATA_TOKEN = os.getenv('DADATA_TOKEN')
 
 # configuring the bot and connecting to the api
 
